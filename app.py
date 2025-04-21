c4871e973c772a13ed13318df3ca233da4875bed
Antonio Feregrino <antonio.feregrino@gmail.com>
2025-04-21 14:41:01 +0100

    Add version management

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from edit import extract_scenes
 
 app = Flask(__name__)
 
+__version__ = "0.1.0"
 OUTPUT_FOLDER = ".output"
 app.config["MAX_CONTENT_LENGTH"] = 500 * 1024 * 1024  # 500MB max file size
 
